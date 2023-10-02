2e5590a66cef0a2dc1a6720fad5759d0b143569e
m0nggh <malcolmong27@gmail.com>
2023-10-02 18:04:41 +0800

    complete exercise-3

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { range } from "../../utils";
+
+function Guess({ value }) {
+  return (
+    <p className="guess">
+      {range(5).map((index) => (
+        <span key={index} className="cell">
+          {value && value[index]}
+        </span>
+      ))}
+    </p>
+  );
+}
+
+export default Guess;
